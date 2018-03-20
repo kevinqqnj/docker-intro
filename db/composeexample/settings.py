7c243ccde4d9fc1771f07f9047a760e9cb0904dc
Kevin Zhang Qing <qing.zhang@ericsson.com>
2018-03-20 14:50:23 +0800

    pg volume

--- db/composeexample/settings.py
+++ db/composeexample/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'db',	# host "db" (172.21.0.2)
         'PORT': 5432,
     },
     'sqlite': {
